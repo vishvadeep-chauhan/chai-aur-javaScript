1d7e2915147d8787891b6162602e867b18324e63
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-04 04:20:49 +0530

    datatypes

---
+++ 01_basics-Lat-const-var/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as a newer version 
+// alert(3 + 3);  work in browser
+
+console.log(3+3);
+
+let name =  "vishv";
+let age = 24
+
+let isLoggedIn = false ;
+
+//number
+//bigint
+//string
+//boolean > true/false
