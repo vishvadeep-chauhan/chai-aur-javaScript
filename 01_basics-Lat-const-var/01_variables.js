3ec86a7d22f6f77ce3a69e5aa6c9ad3371b93199
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-04 03:44:53 +0530

    created variable file and txt for theory  about variable
    questions also in it

---
+++ 01_basics-Lat-const-var/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "vishv@apple.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+let accountState ;
+
+// accountId = 2
+
+console.log(accountId);
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState ]);
+
+
+
