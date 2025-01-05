7a3706110e6eec41bed224b25f5e27ec23447d28
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-06 05:09:10 +0530

    num & maths , & dates

---
+++ 01_basics-Lat-const-var/06_num-and-math.js
@@ -0,0 +1,25 @@
+const score = 400
+
+// console.log(score);
+
+const  balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundereds = 100000
+
+// console.log(hundereds.toLocaleString('en-IN'));
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+
+
+
