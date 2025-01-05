5d3ed5eb934c4e6faf7d94e8b98b660753c22e2b
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-05 08:51:53 +0530

    datatype summery

---
+++ 01_basics-Lat-const-var/datatyps-Summery.js
@@ -0,0 +1,26 @@
+// 7 datatypeas
+
+const score = 100;
+const scorevalue = 100.3;
+const isLoggedin = true;
+
+const outsidetemp = null;
+let useremail ;
+
+const id = Symbol('123');
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 987345820345967486987n
+
+// Array, object , function 
+
+const heros = ["saktiman", "spiderman", "howkey"];
+let myobj = {
+    name: "vishv",
+    age: 22,
+}
+
+console.log(typeof bigNumber);
+
