a68665a43c3608e9c71b4da5e67d7adda278dad8
Vishvadeep Singh Chauhan <vishvadeepsinghchauhan@gmail.com>
2025-01-08 00:20:42 +0530

    enhance array operations with additional methods and examples

--- 02_Basics_adance/01_Array.js
+++ 02_Basics_adance/01_Array.js
@@ -1,5 +1,25 @@
 // array
 
-const myArray = [0, 1, 2, 3, 4, 5, 6, 7]
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7];
 
+console.log(myArr[1]);
+const myHeros = ["saktiman, spiderman"];
+
+const myArr2 = new Array(1, 2, 3, 4);
+console.log(myArr2[1]);
+
+
+myArr.push(6);
+myArr.push(11);
+
+myArr.pop();
+
+myArr.unshift(9)
+myArr.unshift(12)
+
+myArr.shift()
+
+console.log(myArr.includes(9));
+
+console.log(myArr);
 
